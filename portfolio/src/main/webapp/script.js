eb52815c9fe0f6a8892f8781b1b2d987ab4da991
Kerryn <kerryneer98@gmail.com>
2020-06-06 13:28:53 +0800

    Add projects slideshow rendering logic

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,6 +12,42 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// PROJECT SLIDESHOW LOGIC
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("project");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+      slideIndex = 1;
+  }
+  if (n < 1) {
+      slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
 //SPARKLE CURSOR LOGIC
 var colour="random";
 var sparkles=50;
