bd11d215ed74b2b0372aeb0e8c66ee4fded5cb8d
Kerryn <kerryneer98@gmail.com>
2020-06-20 12:48:32 +0800

    Implement createMap function

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -18,6 +18,13 @@ function loadPage() {
     loadSparkleCursor();
 }
 
+// Creates a map and adds it to the page.
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
+
 // Get latest comments from server
 function getLatestComments() {
     const language = document.getElementById('language').value;
