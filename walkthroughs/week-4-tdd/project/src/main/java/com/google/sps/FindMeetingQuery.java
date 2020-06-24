c6684eb900542e1c20be02b6f98b81ff0c49ed23
Kerryn <kerryneer98@gmail.com>
2020-06-24 21:57:25 +0800

    Implement find meeting query logic

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -1,23 +1,80 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Set;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    
+    Collection<TimeRange> timeSlots = new ArrayList<TimeRange>();
+
+    Long requestedDuration = request.getDuration();
+    if (requestedDuration > TimeRange.WHOLE_DAY.duration()) {
+      return timeSlots;
+    }
+
+    // 1. Filter out events that at least 1 attendee needs to attend
+    Collection<Event> clashedEvents = new ArrayList<Event>();
+    Collection<String> attendees = request.getAttendees();
+
+    for (Event event: events) {
+      if (hasClash(event, attendees)) {
+        clashedEvents.add(event);
+      }
+    }
+
+    // 2. Get the remaining time slots without clash
+    timeSlots.add(TimeRange.WHOLE_DAY);
+    
+    for (Event event: clashedEvents) {
+      TimeRange eventTimeSlot = event.getWhen();
+
+      //iterate through each timeslot and update if clash
+      Collection<TimeRange> updatedTimeSlots = new ArrayList<TimeRange>();
+ 
+      for (TimeRange timeSlot: timeSlots) {
+      
+        if (timeSlot.overlaps(eventTimeSlot)) {
+          int start = timeSlot.start();
+          int end = timeSlot.end();
+          int eventStart = eventTimeSlot.start();
+          int eventEnd = eventTimeSlot.end();
+          TimeRange newTimeSlot;
+
+          if (start < eventStart) {
+            newTimeSlot = TimeRange.fromStartEnd(start, eventStart, false);
+            if (newTimeSlot.duration() >= requestedDuration) {
+              updatedTimeSlots.add(newTimeSlot);
+            }
+          }
+
+          if (end > eventEnd) {
+            newTimeSlot = TimeRange.fromStartEnd(eventEnd, end, false);
+            if (newTimeSlot.duration() >= requestedDuration) {
+              updatedTimeSlots.add(newTimeSlot);
+            }
+          }
+
+        } else {
+          updatedTimeSlots.add(timeSlot);
+        }
+      }
+  
+      timeSlots = updatedTimeSlots;
+    } 
+
+    return timeSlots;
+  }
+
+  private Boolean hasClash(Event scheduled, Collection<String> attendees ) {
+    Set<String> eventAttendees = scheduled.getAttendees();
+  
+    for (String attendee: attendees) {
+      if (eventAttendees.contains(attendee)) {
+        return true;
+      } 
+    }
+    return false;
   }
 }
